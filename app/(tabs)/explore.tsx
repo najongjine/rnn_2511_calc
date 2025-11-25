e71843807b045f847a1d8bbbf3b666d48209e150
najongjine <najongjin3@hotmail.com>
2025-11-25 11:08:36 +0900

    .

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,4 +1,4 @@
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function TabTwoScreen() {
   return (
@@ -6,8 +6,23 @@ export default function TabTwoScreen() {
       <View>
         <Text>calc page</Text>
       </View>
+      <View>
+        <TextInput />
+      </View>
     </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  blueText: {
+    color: "blue",
+    fontSize: 24,
+    margin: 10,
+  },
+  orangeInput: {
+    borderColor: "orange",
+    borderWidth: 2,
+    padding: 10,
+    width: "100%",
+  },
+});
