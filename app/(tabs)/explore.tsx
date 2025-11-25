8e39c81aaa35f519479946e4081ae46fe0168363
najongjine <najongjin3@hotmail.com>
2025-11-25 11:43:57 +0900

    .

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -3,14 +3,18 @@ import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function TabTwoScreen() {
   const [myinput, setMyinput] = useState<string>("");
-  /* myinput 이랑 textinput 이랑 연결 해주기 */
+  /* 계산하기 버튼 만들기 */
+
   return (
     <ScrollView>
       <View>
         <Text style={{ color: "blue" }}>calc page</Text>
       </View>
       <View>
-        <TextInput style={styles.orangeInput} />
+        <TextInput onChangeText={setMyinput} style={styles.orangeInput} />
+        <View>
+          <Text>myinput에 잘 담겼니? {myinput}</Text>
+        </View>
       </View>
     </ScrollView>
   );
