02bb5287229564c40d63f6675d3bcf5c674da657
najongjine <najongjin3@hotmail.com>
2025-11-26 10:40:13 +0900

    .

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -17,7 +17,8 @@ export default function TabTwoScreen() {
     evaluate();
   }
   /*
-  testadd() 랑 Button 연결하고, result 값 화면에 표시
+  1. calc를 계산하기 버튼에 연결하고,
+  2. evaluate() 함수를 사용해서 계산을 해보세요 (우리가 타이핑한건 myinput에 담겨 있어요)
    */
   return (
     <ScrollView>
